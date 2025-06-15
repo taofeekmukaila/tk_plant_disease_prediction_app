bf6b772f485555b633902ba46724af69e72b2de2
taofeekmukaila <mukaila36@gmail.com>
2025-06-15 12:06:59 -0500

    update the zoom level

--- pages/2_🪟_Disease_Prediction_Map.py
+++ pages/2_🪟_Disease_Prediction_Map.py
@@ -7,9 +7,6 @@ markdown = """
 
 Use the map below to **draw a polygon** (e.g., field boundary or affected area).
     The map is using **Esri World Imagery** as a basemap.
-
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
 """
 
 st.sidebar.title("About")
@@ -20,7 +17,7 @@ st.sidebar.image(logo)
 st.title("Plant Disease Prediction Map")
 
 # Create the interactive map
-m = leafmap.Map(center=[38.5, -98.0], zoom=5, draw_export=True)
+m = leafmap.Map(center=[38.14306249508979, -102.61118888854982], zoom=15.0, draw_export=True)
 m.add_basemap("Esri.WorldImagery")  # Add satellite imagery
 m.to_streamlit(height=600)
 
